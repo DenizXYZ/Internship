56413ee413ebd29dcfe8d5cf93a25808d9440dc0
Deniz Yurdatapan <deniz@Denizs-MacBook-Pro.local>
2023-05-25 17:25:59 +0200

    fixed error: Google data button now redirects to a google consent screen (user give consent to the app managing and reading the data)

--- securitydetection/googledata/views.py
+++ securitydetection/googledata/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 # Create your views here.
 def index(request):
     import os
-    from google_auth_oauthlib.flow import InstalledAppFlow
-    from googleapiclient.discovery import build
+    import google_auth_oauthlib.flow 
+    import googleapiclient.discovery 
 
     CLIENT_SECRET_FILE = r'/Users/deniz/Documents/Python/security detection(Internship)/securitydetection/client_secret_398946460996-s2nncaimr0a0i4fpvmt7l2dkpr2kigfu.apps.googleusercontent.com.json'
-    # Scopes define the level of access you need
-    SCOPES = ['https://www.googleapis.com/auth/account']
 
-    credentials_path = CLIENT_SECRET_FILE#'''path/to/credentials.json'''
-    flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    CLIENT_SECRET_FILE,
+    scopes=['https://www.googleapis.com/auth/bigquery'])#['https://www.googleapis.com/auth/drive.metadata.readonly'])
 
-    credentials = flow.run_local_server(port=0)
+    
+    flow.redirect_uri = 'https://127.0.0.1:8000/auth/google'
 
-    service = build('account', 'v1', credentials=credentials)
-    accounts = service.accounts().list().execute()
-    accounts2=[]
-    for account in accounts2['accounts']:
-        accounts2.append(f"Account ID: {account['accountId']}")
+    authorization_url, state = flow.authorization_url(access_type='offline',include_granted_scopes='true')
 
-    return HttpResponse(accounts2)
+    return redirect(authorization_url)
